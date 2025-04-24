9d469e8cb7ad3289cacf5c56a2bc2480552fe581
Piyush Verma <piyush.vermapv38@gmail.com>
2025-04-24 06:11:44 +0530

    Add files via upload

--- 05_Solutions.py
+++ 05_Solutions.py
@@ -14,3 +14,10 @@ for num in numbers:
     with open(f"table/table_{num}.txt", "w") as f:
         f.write("\n".join(table))
         
+# OR
+
+n = int(input("Enter a number whose table you wish to see: "))
+
+table : list = [n * i for i in range(0,11)]
+with open("tables.txt", "a") as f:
+    f.write(f"Table of {n} is: {str(table)} \n")
